93f4d1f5a2012b02b32058a596dce5cdd814d5dd
Maurice Le Cordier <mauricelecordier@gmail.com>
2022-03-21 15:34:58 +0200

    added an example of the running program in main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mauriceLC92/virtual-computer-system/gmachine"
+)
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("Gmachine!")
+	g := gmachine.New()
+
+	g.RunProgram([]uint64{
+		uint64(gmachine.SETA), 5,
+		uint64(gmachine.HALT),
+	})
 }
