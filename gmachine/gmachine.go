56971be46647708654f318764867a735a018280b
Maurice Le Cordier <mauricelecordier@gmail.com>
2022-03-21 15:07:50 +0200

    implemented the run function

--- gmachine/gmachine.go
+++ gmachine/gmachine.go
@@ -5,15 +5,39 @@ package gmachine
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
 
+// Opcode is the numeric code which represents an instruction
+type Opcode uint64
+
+const (
+	HALT Opcode = iota
+	NOOP
+)
+
 type Gmachine struct {
-	P      uint64
+	// P is the program counter and holds the memory address of the next instruction
+	// to execute.
+	P uint64
+	// A is the accumulator which will keep track of our current result
+	A      uint64
 	Memory []uint64
 }
 
+// New instantiates a new Gmachine in it's initial state.
 func New() *Gmachine {
-	mem := make([]uint64, 1024)
+	mem := make([]uint64, DefaultMemSize)
 	return &Gmachine{
-		P:      uint64(0),
 		Memory: mem,
+	}
+}
+
+func (gm *Gmachine) Run() {
+	for {
+		nextInstuction := gm.Memory[gm.P]
+		gm.P += 1
+		switch nextInstuction {
+		case uint64(HALT):
+			return
+		case uint64(NOOP):
+		}
 	}
 }
--- gmachine/gmachine_test.go
+++ gmachine/gmachine_test.go
@@ -18,9 +18,38 @@ func TestNew(t *testing.T) {
 	if wantP != g.P {
 		t.Errorf("want initial P value %d, got %d", wantP, g.P)
 	}
+	var wantA uint64 = 0
+	if wantA != g.A {
+		t.Errorf("want initial P value %d, got %d", wantP, g.P)
+	}
 	var wantMemValue uint64 = 0
 	gotMemValue := g.Memory[gmachine.DefaultMemSize-1]
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
+	}
+}
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.Run()
+
+	wantP := 1
+	gotP := g.P
+	if wantP != int(gotP) {
+		t.Errorf("wanted counter value %d but got %d instead", wantP, gotP)
+	}
+}
+
+func TestNOOP(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.P = uint64(gmachine.NOOP)
+	g.Run()
+
+	wantP := 2
+	gotP := g.P
+	if wantP != int(gotP) {
+		t.Errorf("wanted counter value %d but got %d instead", wantP, gotP)
 	}
 }
